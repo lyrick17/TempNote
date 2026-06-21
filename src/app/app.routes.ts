e8cbd44ba0f33b6d2bdb2c124745d231794a1687
lyrick <lyrickjonson@gmail.com>
2026-06-21 17:09:54 +0800

    v2.3 - added privacy policy, terms conditions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { TermsAndConditions } from './features/terms-and-conditions/terms-and-conditions';
+import { PrivacyPolicy } from './features/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: Home },
   // { path: 'about', component: About},
-  // { path: 'terms-and-conditions', component: TermsAndConditions},
-  // { path: 'privacy-policy', component: PrivacyPolicy},
+  { path: 'terms-and-conditions', component: TermsAndConditions },
+  { path: 'privacy-policy', component: PrivacyPolicy },
   { path: '**', redirectTo: '' },
 ];
--- src/app/core/shell/navigation/navigation.ts
+++ src/app/core/shell/navigation/navigation.ts
@@ -1,17 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { HomeTabState } from '../../../features/home/services/home-tab-state';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navigation.html',
   styleUrl: './navigation.css',
 })
 export class Navigation {
+  private router = inject(Router);
   homeTab = inject(HomeTabState);
 
   setHomeTab(tab: 'scratchpad' | 'stash') {
     this.homeTab.setTab(tab);
+  }
+
+  currentUrl(): string {
+    return this.router.url;
+  }
+
+  isHome(): boolean {
+    return this.router.isActive('/', {
+      paths: 'exact',
+      queryParams: 'ignored',
+      fragment: 'ignored',
+      matrixParams: 'ignored',
+    });
   }
 }
